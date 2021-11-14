6c561f1d3aca1998c2984b4a93a017cfbebaad49
Cheetah0723 <kimuraa.hideyo@gmail.com>
2021-11-13 20:54:58 -0600

    added factors to players list

--- src/components/PlayerList.vue
+++ src/components/PlayerList.vue
@@ -1,29 +1,29 @@
 <template>
   <div v-if="!homeTeam">Loading...</div>
   <div class="container mb-5">
-  <div class="row">
-    <div class="col-5">
-      <h3 v-if="event" class="m-5">{{ event['home-team'].name }} </h3>
+    <div class="row">
+      <div class="col-5">
+        <h3 v-if="event" class="m-5">{{ event['home-team'].name }} </h3>
+      </div>
+      <div class="col-2">
+        <h3 v-if="event" class="m-5">vs</h3>
+      </div>
+      <div class="col-5">
+        <h3 v-if="event" class="m-5"> {{ event['away-team'].name }}</h3>
+      </div>
     </div>
-    <div class="col-2">
-      <h3 v-if="event" class="m-5">vs</h3>
-    </div>
-    <div class="col-5">
-      <h3 v-if="event" class="m-5"> {{ event['away-team'].name }}</h3>
+    <div class="row">
+      <div class="col-5">
+        <h5 v-if="event">Guessing Score: {{ event['home-team']['guessing-score'] }}</h5>
+      </div>
+      <div class="col-2">
+        <h5 v-if="event"></h5>
+      </div>
+      <div class="col-5">
+        <h5 v-if="event">Guessing Score: {{ event['away-team']['guessing-score']  }}</h5>
+      </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-5">
-      <h5 v-if="event">Guessing Score: {{ event['home-team']['guessing-score'] }}</h5>
-    </div>
-    <div class="col-2">
-      <h5 v-if="event"></h5>
-    </div>
-    <div class="col-5">
-      <h5 v-if="event">Guessing Score: {{ event['away-team']['guessing-score']  }}</h5>
-    </div>
-  </div>
-</div>
   <div class="row">
     <div class="col-6" v-if="homeTeam">
       <h4>Home Team</h4>
@@ -35,13 +35,28 @@
         <tr>
           <th>#</th>
           <th colspan="2">Player Name</th>
+          <th>Factors</th>
           <th>Guessing Score</th>
           <th>Boost</th>
         </tr>
         <tr v-for="(player, index) in homeTeam.players" :key="index">
           <td>{{index + 1}}</td>
           <td colspan="2">{{player['player-name']}}</td>
-          <td>{{player['guessing-score']['score'].toFixed(5)}}</td>
+          <td>
+            <table class="table">
+              <tbody>
+                <tr class="inline">
+                  <td> <i class="bi bi-house-fill"></i> {{player['algorithms']['home-away-weighting']['scaling_factor'].toFixed(2)}} </td>
+                  <td> <i class="bi bi-bullseye"></i> {{player['algorithms']['goals-against-average']['scaling_factor'].toFixed(2)}} </td>
+                </tr>
+                <tr>
+                  <td><i class="bi bi-x-circle-fill"></i> {{player['algorithms']['x-factor']['scaling_factor'].toFixed(2)}} </td>
+                  <td><i class="bi bi-calendar-week"></i> {{player['algorithms']['schedule']['scaling_factor'].toFixed(2)}} </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+          <td>{{player['guessing-score']['score'].toFixed(2)}}</td>
           <td>{{player['guessing-score']['boost']}}</td>
         </tr>
       </table>
@@ -66,19 +81,25 @@
           <td>
             <table class="table">
               <tbody>
+                <tr class="inline">
+                  <td> <i class="bi bi-house-fill"></i> {{player['algorithms']['home-away-weighting']['scaling_factor'].toFixed(2)}} </td>
+                  <td> <i class="bi bi-bullseye"></i> {{player['algorithms']['goals-against-average']['scaling_factor'].toFixed(2)}} </td>
+                </tr>
                 <tr>
-                  <td>{{player['algorithms']['home-away-weighting']['scaling-factor']}} </td>
+                  <td><i class="bi bi-x-circle-fill"></i> {{player['algorithms']['x-factor']['scaling_factor'].toFixed(2)}} </td>
+                  <td><i class="bi bi-calendar-week"></i> {{player['algorithms']['schedule']['scaling_factor'].toFixed(2)}} </td>
                 </tr>
               </tbody>
             </table>
           </td>
           <td>{{player['guessing-score']['boost']}}</td>
-          <td>{{player['guessing-score']['score'].toFixed(5)}}</td>
+          <td>{{player['guessing-score']['score'].toFixed(2)}}</td>
         </tr>
 
       </table>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -122,7 +143,7 @@ export default {
       for (const player of team.players) {
         total = total + player['guessing-score'].score
       }
-      return total.toFixed(5)
+      return total.toFixed(2)
     }
   }
 }
